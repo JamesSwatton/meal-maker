a56d60548339718437d3927b9c289796e27d6f90
JamesSwatton <jamessswatton@gmail.com>
2019-10-31 10:27:02 +0000

    add generateRandomMeal

--- meal_maker.js
+++ meal_maker.js
@@ -48,22 +48,31 @@ const menu = {
         }
     },
     getRandomDishFromCourse(courseName) {
-        const dishes = this.courses;
+        const dishes = this.courses[courseName];
         const randomIndex = Math.floor(Math.random() * dishes.length);
         return dishes[randomIndex];
     },
     generateRandomMeal() {
         const appetizer = this.getRandomDishFromCourse('appetizers');
-        const main = this.getRandomDishFromCourse('main');
-        const dessert = this.getRandomDishFromCourse('dessert');
+        const main = this.getRandomDishFromCourse('mains');
+        const dessert = this.getRandomDishFromCourse('desserts');
         const mealTotalPrice = appetizer.price + main.price + dessert.price;
-        return `Your appetizer will be ${appetizer}, followed by ${main} for your main and ${dessert} for dessert. The total cost will be ${mealTotalPrice}.`;
+        return `Your appetizer will be ${appetizer.name}, followed by ${main.name} for your main and ${dessert.name} for dessert. The total cost will be ${mealTotalPrice}.`;
     }
 };
 
 menu.addDishToCourse('appetizers', 'soup', 5);
 menu.addDishToCourse('appetizers', 'salad', 4);
 menu.addDishToCourse('appetizers', 'pate', 6);
-menu.addDishToCourse('main', 'pasta', 9);
-console.log(menu._courses.appetizers);
-console.log(menu.courses);
+menu.addDishToCourse('mains', 'pasta', 8);
+menu.addDishToCourse('mains', 'pizza', 9);
+menu.addDishToCourse('mains', 'pie', 10);
+menu.addDishToCourse('desserts', 'ice cream', 4);
+menu.addDishToCourse('desserts', 'cake', 4);
+menu.addDishToCourse('desserts', 'apple pie', 5);
+
+// console.log(menu._courses.appetizers);
+// console.log(menu.courses);
+
+const meal = menu.generateRandomMeal();
+console.log(meal);
